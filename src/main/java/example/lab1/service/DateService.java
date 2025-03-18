e2cfefc4e2263c9acf6bd341ad026a8311b0b975
Maga-nan <160400346+Maga-nan@users.noreply.github.com>
2025-03-18 15:13:42 +0300

    DateService.java

--- src/main/java/example/lab1/service/DateService.java
+++ src/main/java/example/lab1/service/DateService.java
@@ -11,7 +11,15 @@ public class DateService {
     public static String getDateInCurrentTimeZone(long timestamp) {
         return DateDAO.getDateInCurrentTimeZone(timestamp);
     }
+     @GetMapping("/convertDate")
+    public ResponseEntity<?> convertDate(@RequestParam("timestamp") String timestamp) {
+        try {
+            long timestampLong = Long.parseLong(timestamp);
 
+
+            if (timestampLong < 0) {
+                return ResponseEntity.badRequest().body("Invalid timestamp value");
+            }
     public static String getDateInGMT(long timestamp) {
         return dateDAO.getDateInGMT(timestamp);
     }

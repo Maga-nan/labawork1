621a6cd24c1858c795f2206d96e0f8f8d858d571
Maga-nan <160400346+Maga-nan@users.noreply.github.com>
2025-03-18 15:14:36 +0300

    Update DateService.java

--- src/main/java/example/lab1/service/DateService.java
+++ src/main/java/example/lab1/service/DateService.java
@@ -2,7 +2,9 @@ package example.lab1.service;
 
 import example.lab1.dao.DateDAO;
 import org.springframework.stereotype.Service;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @Service
 public class DateService {
